23c16565491f1fcdad2de84a7e5af51148851099
PlotoZypresse <janlucca1401@gmail.com>
2024-12-19 22:51:33 +0100

    added a progress bar and a sound is played at the end of the timer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::thread;
 use std::time::Duration;
 use std::io;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
 
 fn main() {
     // get user input
@@ -14,10 +19,37 @@ fn timer(time: u64) {
     // convert the input time to seconds
     let time_to_sec = time * 60;
 
-    //set the timer
-    thread::sleep(Duration::from_secs(time_to_sec));
+    let bar = ProgressBar::new(time_to_sec);
+    bar.set_style(
+        ProgressStyle::with_template("{spinner:.cyan} 🍅 [Time Remainng {bar:.40.cyan/gray}] {pos}/{len}s")
+        .unwrap()
+        .progress_chars("█▓▒░")
+    );
 
-    println!("{time} minutes have passed");
+    for _ in 0..time_to_sec {
+        thread::sleep(Duration::from_secs(1));
+        bar.inc(1);
+    }
+
+    //set the timer
+    //thread::sleep(Duration::from_secs(time_to_sec));
+    
+    // Get an output stream handle to the default sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // load the sound file
+    let file = BufReader::new(File::open("sounds/pomodoroFinish.mp3").unwrap());
+    //decode sound file into a source
+    let source = Decoder::new(file).unwrap();
+    //Play the sound
+    
+   
+    println!("✅ Pomodoro Timer completed");
+
+    let _ = stream_handle.play_raw(source.convert_samples());
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    //bar.finish_with_message() :wq
+    //
 } 
 
 fn user_input() -> u64 {
