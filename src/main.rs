4daa9a5cf90c727567ee40ad8d37f31936a5263f
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-01-10 16:11:49 +0100

    Add first files to persistent storage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod menu;
+mod storage;
 mod timers;
 mod ui;
 
---
+++ src/storage.rs
@@ -0,0 +1,35 @@
+//! # Storage
+//! This file contains the necessary functions to create persistent storage for
+//! Tomato.
+//!
+//! Tomato uses a JSON file to store the sessions a user has had.
+
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Session {
+    pub timestamp: String, // Maybe use chrono?
+    pub id: u32,
+    pub work_time: u32,
+    pub break_time: u32,
+}
+
+struct Storage {
+    storage_file: String,
+}
+
+impl Session {
+    pub fn new() -> Session {}
+
+    pub fn to_json() -> String {
+        serde_json::to_string(&self).unwrap()
+    }
+}
+
+impl Storage {
+    pub fn new(path: String) {
+        Storage { storage_file: path }
+    }
+}
