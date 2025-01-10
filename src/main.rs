9f856c031d5caf87af9898e7d42b1b3ca168e662
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-01-10 11:34:38 +0100

    Removed doc comment from Main
    When moving it, I forgot to remove it from the main file.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,6 @@ mod menu;
 mod timers;
 mod ui;
 
-/// Represents the values of a timer, as well as the time worked in minutes.
-///
-/// # Examples
-///
-/// ```
-/// let timer = Timer { work_time: 25, break_time: 10, time_worked 0 };
-/// println!("Current work/break distribution: {}/{}", timer.work_time, timer.break_time);
-/// println!("Time worked in total: {}", timer.time_worked);
-/// assert_eq!(timer.work_time, 25);
-/// assert_eq!(timer.break_time, 10);
-/// assert_eq!(timer.time_worked, 0);
-/// ```
-
 fn main() {
     ui::ui_loop();
 }
