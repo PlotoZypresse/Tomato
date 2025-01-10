b9c37cfea8ddc2d95f8c990bb278dcc533f7d9ac
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-01-10 11:08:56 +0100

    Move doc comment
    Doc comment was mistakenly let in at earlier commit, at the wrong place.
    Now it has been moved to be at the corresponding struct it comments.

--- src/timers.rs
+++ src/timers.rs
@@ -5,6 +5,18 @@ use std::io::BufReader;
 use std::thread;
 use std::time::Duration;
 
+/// Represents the values of a timer, as well as the time worked in minutes.
+///
+/// # Examples
+///
+/// ```
+/// let timer = Timer { work_time: 25, break_time: 10, time_worked 0 };
+/// println!("Current work/break distribution: {}/{}", timer.work_time, timer.break_time);
+/// println!("Time worked in total: {}", timer.time_worked);
+/// assert_eq!(timer.work_time, 25);
+/// assert_eq!(timer.break_time, 10);
+/// assert_eq!(timer.time_worked, 0);
+/// ```
 pub struct Timer {
     pub work_time: u64,
     pub break_time: u64,
