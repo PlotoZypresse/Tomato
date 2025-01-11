10869f3ef83758d6fd6d930e3c6606465a08a28f
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-01-11 11:53:33 +0100

    Add test for read and writing to file
    Also made the functions work for the test.

--- src/storage.rs
+++ src/storage.rs
@@ -7,9 +7,38 @@
 use chrono::prelude::*;
 use chrono::serde::ts_seconds; // Allows for seralization with Chrono Timestamps
 use chrono::{DateTime, Utc};
+use home::home_dir;
 use serde::{Deserialize, Serialize};
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
+
+/// Using the `home` crate, finds the home folder for the current user.
+///
+/// ## Returns
+/// The home directory. Panics if this cannot be found.
+fn get_home_path() -> String {
+    let mut path: String;
+
+    match home_dir() {
+        Some(home_path) => {
+            path = home_path.into_os_string().into_string().unwrap();
+        }
+        None => panic!("Impossible to get home dir."),
+    }
+
+    path
+}
+
+/// Checks whether the folder exists.
+///
+/// ## Returns
+/// True if the folder exists. False otherwise.
+fn folder_exists(folder: String) -> bool {
+    let path = format!("{}/{}", get_home_path(), folder);
+    Path::new(&path).exists()
+}
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Session {
@@ -19,8 +48,10 @@ pub struct Session {
     pub break_time: u32,
 }
 
+/// Holds the variable containing the path to the storage file.
 struct Storage {
     storage_file: String,
+    folder: String,
 }
 
 impl Session {
@@ -56,8 +87,73 @@ impl Session {
 }
 
 impl Storage {
-    pub fn new(path: String) -> Storage {
-        Storage { storage_file: path }
+    /// Creates a new Storage struct.
+    ///
+    /// ## Arguments
+    /// * path
+    /// The name of the file, without any prefix. E.g. "file.txt"
+    ///
+    /// ## Returns
+    /// A storage struct containing the variable `storage_file` with the value
+    /// of argument appended to the default path prefix ("~/.tomato/")
+    pub fn new(folder: Option<String>, path: String) -> Storage {
+        Storage {
+            storage_file: format!(
+                "{}/{}/{}",
+                get_home_path(),
+                folder.clone().unwrap_or(".tomato".to_string()),
+                path
+            ),
+            folder: folder.unwrap_or(".tomato".to_string()),
+        }
+    }
+
+    /// Writes to `storage_file`.
+    ///
+    /// ## Returns
+    /// A Result value. Ok(()) if no problems occured, otherwise Err.
+    pub fn write(&self, folder: Option<String>, text: String) -> std::io::Result<()> {
+        if !folder_exists(folder.unwrap_or(".tomato".to_string())) {
+            let path = format!("{}/{}/", get_home_path(), self.folder);
+            match fs::create_dir(path) {
+                Ok(_) => (),
+                Err(v) => panic!("{}", v),
+            }
+        }
+
+        // File::create creates a file if it does not exist.
+        // If it does exist, it truncates the file.
+        let mut file = File::create(self.storage_file.clone())?;
+
+        file.write(text.as_bytes())?;
+
+        Ok(())
+    }
+
+    // TODO: If this should only be called in tests, should it maybe only be
+    // defined there?
+    /// Removes `storage_file`. This should only be called in tests.
+    ///
+    /// ## Returns
+    /// A Result value. Ok(()) if successfully deleted file, err otherwise.
+    fn remove_file(&self) -> std::io::Result<()> {
+        let mut file = fs::remove_file(self.storage_file.clone())?;
+        Ok(())
+    }
+
+    /// Reads from `storage_file`.
+    ///
+    /// ## Returns
+    /// A Result value. Ok(String) containing the contents, if succesful.
+    /// Err otherwise.
+    pub fn read(&self) -> std::io::Result<String> {
+        let mut file = File::open(self.storage_file.clone())?;
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        // TODO: Do we need Clone here?
+        Ok(contents.clone())
     }
 }
 
@@ -88,5 +184,43 @@ mod tests {
         let deserialized_session = Session::from_json(&json_str).expect("Invalid JSON");
 
         assert_eq!(session, deserialized_session);
+    }
+
+    #[test]
+    fn create_and_write_to_file() {
+        // Remove the directory first, to test if it creates the directory
+        // correctly.
+        let path = format!("{}/.tomato_test/", get_home_path());
+
+        match fs::remove_dir_all(path) {
+            Ok(_) => (),
+            Err(v) =>
+                println!("Folder did not exist at the start. If this is not the first time running this test, something is wrong. Error: {}", v),
+        }
+
+        let name = "test.txt".to_string();
+
+        let expected_path = format!("{}/.tomato_test/{}", get_home_path(), name).to_string();
+        let storage = Storage::new(Some(".tomato_test".to_string()), name.clone());
+
+        // Make sure that it has the correct prefix
+        assert_eq!(storage.storage_file, expected_path);
+
+        let write_value = String::from("Æether Åland Øndre");
+
+        match storage.write(Some(".tomato_test".to_string()), write_value.clone()) {
+            Ok(_) => (),
+            Err(v) => panic!("{}. Check path {}", v, expected_path),
+        }
+
+        match storage.read() {
+            Ok(v) => assert_eq!(v, write_value),
+            Err(v) => panic!("{}", v),
+        }
+
+        match storage.remove_file() {
+            Ok(_) => (),
+            Err(v) => panic!("{}", v),
+        }
     }
 }
