b6c23293128452f6afa68ea14924ea8c7388d6a9
PlotoZypresse <janlucca1401@gmail.com>
2025-02-10 13:26:52 +0100

    refactor: changed .icon in notify.rs to hopefully display firefox (like in the example)

--- src/notify.rs
+++ src/notify.rs
@@ -4,7 +4,7 @@ pub fn send_notification_work() {
     let _ = Notification::new()
         .summary("Work is done!!")
         .body("Well done being productive")
-        .icon("img/tomato.jpeg")
+        .icon("firefox")
         .show();
 }
 
@@ -12,6 +12,6 @@ pub fn send_notification_break() {
     let _ = Notification::new()
         .summary("Break is done!")
         .body("Get back to work")
-        .icon("img/tomato.jpeg")
+        .icon("firefox")
         .show();
 }
