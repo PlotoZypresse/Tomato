017287df011d61fd2263ff558a5271546bf447f8
PlotoZypresse <janlucca1401@gmail.com>
2025-02-13 10:11:18 +0100

    minor

--- src/notify.rs
+++ src/notify.rs
@@ -1,16 +1,47 @@
+use crate::{
+    settings::{Notifications, Settings},
+    storage::Storage,
+};
 use notify_rust::Notification;
 
+fn load_settings() -> Settings {
+    let folder = String::from(".tomato");
+    let file_name = String::from("settings.json");
+
+    let storage = Storage::new(Some(folder), file_name.clone());
+
+    let contents = storage.read().unwrap_or_else(|_| {
+        let settings = Settings::new(25, 5, Notifications::default());
+        match storage.write(settings.to_json()) {
+            Ok(_) => (),
+            Err(v) => panic!(
+                "An error occured while writing the settings to the settings file: {}",
+                v
+            ),
+        }
+        "{}".to_string()
+    });
+
+    if contents.is_empty() || contents == "{}" {
+        Settings::new(25, 5, Notifications::default())
+    } else {
+        Settings::from_json(&contents).expect("Could not parse the contents of file.")
+    }
+}
+
 pub fn send_notification_work() {
+    let settings = load_settings();
     let _ = Notification::new()
-        .summary("Work is done!!")
+        .summary(&settings.notification.work_msg)
         .body("Well done being productive")
         .icon("firefox")
         .show();
 }
 
 pub fn send_notification_break() {
+    let settings = load_settings();
     let _ = Notification::new()
-        .summary("Break is done!")
+        .summary(&settings.notification.break_msg)
         .body("Get back to work")
         .icon("firefox")
         .show();
